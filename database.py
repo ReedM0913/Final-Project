5eaedda0f72c27b85a2d00c04b51e519e1dfd16a
ReedM0913 <137439766+ReedM0913@users.noreply.github.com>
2023-06-25 10:31:43 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
-from bearAI.py import bear_counter
-from bearAI.py import bear_dictionary
+from ai.py import pet_counter
+from ai.py import pet_dictionary
 
 def printDatabase(counter, dictionary):
-    black_bears = dictionary["black bear"]
-    brown_bears = dictionary["brown bear"]
-    x = (f"Count of total bears: {counter}\n") + (f"Count of brown bears: {brown_bears}\n") + (f"Count of black bears: {black_bears}")
+    dog = dictionary["dog"]
+    cat = dictionary["cat"]
+    x = (f"Count of total pets: {counter}\n") + (f"Count of dogs: {dogs}\n") + (f"Count of cats: {cats}")
     return x
   
  
-bear = printDatabase(bear_counter, bear_dictionary)
-bear()
+pet = printDatabase(pet_counter, pet_dictionary)
+pet()
