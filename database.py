8e41e3e6e98969e52fd7f0a39b745474c109e65f
ReedM0913 <137439766+ReedM0913@users.noreply.github.com>
2023-06-25 10:27:24 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
-from bearAI.py import bear_counter
-from bearAI.py import bear_dictionary
+from ai.py import day_counter
+from ai.py import night_counter
+from ai.py import day_dictionary
+from ai.py import night_dictionary
 
 def printDatabase(counter, dictionary):
-    black_bears = dictionary["black bear"]
-    brown_bears = dictionary["brown bear"]
-    x = (f"Count of total bears: {counter}\n") + (f"Count of brown bears: {brown_bears}\n") + (f"Count of black bears: {black_bears}")
+    day = dictionary["day"]
+    night = dictionary["night"]
+    x = (f"Count of day: {counter}\n") + (f"Count of night: {night}\n")
     return x
   
  
-bear = printDatabase(bear_counter, bear_dictionary)
-bear()
+dvn = printDatabase(day_counter, night_counter, day dictionary, night_dictionary)
+dvn()
