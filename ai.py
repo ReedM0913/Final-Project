0e86ac35817cf39a813937a4450dff23670d6700
ReedM0913 <137439766+ReedM0913@users.noreply.github.com>
2023-07-05 15:39:26 -0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import jetson.inference
 import jetson.utils
 
@@ -6,27 +7,24 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str, help="filename of the image to process")
-parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect. (see --help for others)")
 opt = parser.parse_args()
 
 
 img = jetson.utils.loadImage(opt.filename)
 
-net = jetson.inference.imageNet(opt.network)
+net = jetson.inference.imageNet(argv=['--model=models/dayornight/resnet18.onnx', '--input-blob=input_0', '--output_blob=output_0', '--labels=data/dayornight/labels.txt'])
 
 class_idx, confidence = net.Classify(img)
 
 class_desc = net.GetClassDesc(class_idx)
+if class_idx == 0:
+    class_desc="Day"
+elif class_idx == 1:
+    class_desc="Night"  
+else:
+    class_desc="other"
 
-pet_counter = 0
-pet_dictionary = {"dog": 0, "cat": 0}
 
-if "pet" or "Pet" in class_desc:
-    pet_counter += 1
-    if "dog" in class_desc:
-        pet_dictionary["dog"] += 1
-    elif "cat" in class_desc:
-        pet_dictionary["cat"] += 1
 
 
 print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))
